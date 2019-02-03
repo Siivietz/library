9b9b65c094bab66869aa74e2f14fc2905eacb8d1
Lukasz Krolak <diin@wp.pl>
2019-02-03 23:06:22 +0100

    Added method "addBook"

--- src/main/java/com/test/library/controller/AdminController.java
+++ src/main/java/com/test/library/controller/AdminController.java
@@ -4,8 +4,12 @@ import com.test.library.model.Book;
 import com.test.library.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.swing.*;
 
 @Controller
 public class AdminController {
@@ -17,13 +21,19 @@ public class AdminController {
     public String getAdminPage() {
         return "admin";
     }
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String getMainPage(Model model) {
+        model.addAttribute("books", bookService.findAll());
+
+        return "admin";
+    }
 
     @RequestMapping(value = "/admin", method = RequestMethod.POST)
     public void addBook(Book book) {
         bookService.save(book);
     }
 
-    @RequestMapping(value = "/admin/delete", method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/admin")
     public void deleteBook(Long id) {
         bookService.deleteBook(id);
     }
